15e51a8dcb0d4dc4118c28143682cefcebf81e5f
selciya20102005 <selciya.k2005@gmail.com>
2026-09-11 15:34:56 +0530

    guide 3 assignment

---
+++ student_management/www/articles.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+def get_context(context):
+    context.articles = frappe.get_all(
+        "Articles",
+        filters={"status": "Published"},
+        fields=["title", "name"]
+    )
